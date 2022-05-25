27ff52e3224e19246d53c1c131dea598234dba41
Haven Fricke <robslaven@live.com>
2022-05-25 12:53:57 -0600

    font setup

--- 116westFlowState.client/src/App.vue
+++ 116westFlowState.client/src/App.vue
@@ -7,7 +7,7 @@
   </main>
   <footer>
     <div class="bg-light text-dark text-center p-4">
-      
+      Made with love, by Haven Fricke 
     </div>
   </footer>
 </template>
@@ -26,5 +26,15 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
+@import url('https://fonts.googleapis.com/css2?family=Hind:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Hind:wght@300&family=Roboto+Condensed:wght@300&display=swap');
+
+h1, h2, h3, h4, h5, h6, div {
+  font-family: 'Hind', sans-serif
+}
+p, a, ul, li{
+font-family: 'Hind', sans-serif;
+font-family: 'Roboto Condensed', sans-serif;
+}
 
 </style>
