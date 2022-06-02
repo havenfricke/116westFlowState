e43eda38f943ba8a0f8240fffe0231439931b72d
Haven Fricke <robslaven@live.com>
2022-06-02 14:41:17 -0600

    small push

--- 116westFlowState.client/src/components/OffCanvas.vue
+++ 116westFlowState.client/src/components/OffCanvas.vue
@@ -7,12 +7,12 @@
   >
     <div class="offcanvas-header">
       <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">
-        <slot name="header"></slot>
+        <slot name="header"><!-- Header OffCanvas slot --></slot>
       </h5>
       <!-- <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
     </div>
     <div class="offcanvas-body">
-      <slot name="body"></slot>
+      <slot name="body"><!-- Footer OffCanvas slot --></slot>
     </div>
   </div>
 </template>
