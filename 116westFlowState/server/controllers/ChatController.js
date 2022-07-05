a9ef914004dd0abb2830f9472f14d79ee6864490
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-05 15:30:22 -0600

    getChatbyId setup in controller

--- 116westFlowState/server/controllers/ChatController.js
+++ 116westFlowState/server/controllers/ChatController.js
@@ -8,11 +8,22 @@ export class ChatController extends BaseController {
     this.router
     .use(Auth0Provider.getAuthorizedUserInfo)
     .get('', this.getAllChats)
+    .get('/:id', this.getChatById)
  }
     async getAllChats(req, res, next) {
         try {
             const chats = await chatService.getAllChats(req.query)
             res.send(chats)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getChatById(req, res, next) {
+        try {
+            req.body.id = req.params.id
+            const chat = await chatService.getchatById(req.params.id)
+            res.send(chat)
         } catch (error) {
             next(error)
         }
--- 116westFlowState/server/services/ChatService.js
+++ 116westFlowState/server/services/ChatService.js
@@ -1,4 +1,7 @@
 class ChatService{
+    getchatById(id) {
+        throw new Error("Method not implemented.");
+    }
     async getAllChats(query = {}) {
         throw new Error("Method not implemented.");
     }
