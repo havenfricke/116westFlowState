00cf74b82dbf16ae073b7254eb385542f3f88efc
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-05 15:11:31 -0600

    servers setup for chat systems

---
+++ 116westFlowState/server/controllers/ChatController.js
@@ -0,0 +1,12 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController";
+
+export class ChatController extends BaseController {
+ constructor(){
+    super('chat')
+    this.router
+    .use(Auth0Provider.getAuthorizedUserInfo)
+    .get('', )
+ }
+
+}
