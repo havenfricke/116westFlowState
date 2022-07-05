269c920df03659bb5a2fd5ab8f9b491f0c311127
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-05 16:38:44 -0600

    homepage height adjusted

--- 116westFlowState.client/src/pages/HomePage.vue
+++ 116westFlowState.client/src/pages/HomePage.vue
@@ -3,7 +3,12 @@
     <div class="row parallax d-flex justify-content-center">
       <div class="m-1"></div>
       <div class="col-lg-5 col-md-8 col-sm-10 text-center text-dark">
-        <h1 class="bg-light p-3 text-center shadow rounded-pill"><div class="mt-2">Welcome to FlowState<h4>A Place for productivity</h4></div></h1>
+        <h1 class="bg-light p-3 text-center shadow rounded-pill">
+          <div class="mt-2">
+            Welcome to FlowState
+            <h4>A Place for productivity</h4>
+          </div>
+        </h1>
       </div>
     </div>
   </div>
@@ -11,17 +16,17 @@
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
 
 <style scoped lang="scss">
 .parallax {
-  /* The image used */  
+  /* The image used */
   background-image: url("../assets/img/3173341.jpg");
 
   /* Set a specific height */
-  min-height: 100vh;
+  min-height: 85vh;
 
   /* Create the parallax scrolling effect */
   background-attachment: fixed;
