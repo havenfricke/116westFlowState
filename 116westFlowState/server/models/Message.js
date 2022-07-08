be29205078d7b07f7280021ef8c1a524461c72fc
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-08 13:01:50 -0600

    message model setup

--- 116westFlowState/server/models/Message.js
+++ 116westFlowState/server/models/Message.js
@@ -3,6 +3,23 @@ const Schema = mongoose.Schema
 
 export const MessageSchema = new Schema(
     {
-        
-    }
+        body: {type: String, required: true},
+        creatorId : {type: Schema.Types.ObjectId, required: true, ref: 'Account'},
+        chatId: {type: Schema.Types.ObjectId, required: true, ref: 'Chat'}
+    },
+    { timestamps: true, toJSON: {virtuals: true} }
 )
+
+MessageSchema.virtual('creator',{
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Account'
+})
+
+MessageSchema.virtual('chat', {
+    localField: 'offerId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Chat'
+})
