10ff1543e458e7dcca887c37639e0f11c69c5f85
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-08 14:21:27 -0600

    message sockets on service layer setup

--- 116westFlowState/server/services/MessageService.js
+++ 116westFlowState/server/services/MessageService.js
@@ -1,10 +1,14 @@
 import { dbContext } from "../db/DbContext";
-
+import { socketProvider } from "../SocketProvider"
 class MessageService {
     async createMessage(body) {
         const message = await dbContext.Messages.create(body)
         await message.populate('creator')
         await message.populate('chat')
+        socketProvider.messageRoom('chat-' + body.chatId, 'new:message', message)
+        const offerNotify = await dbContext.Chats.findById(body.chatId)
+        socketProvider.messageUser(offerNotify.creatorId.toString(), 'new:notify', { message: `${message.creator.name} just messaged you`, offerNotify})
+        return message
     }
     async getChatMessages(id) {
         const messages = await dbContext.Messages.find({ chatId: id }).populate('creator').populate('chat');
