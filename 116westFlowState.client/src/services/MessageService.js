9b86fef803baacc77c4ba3a9f309c8097c71f784
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2022-07-11 11:32:01 -0600

    routes for chats/message rdy 4 debug

--- 116westFlowState.client/src/services/MessageService.js
+++ 116westFlowState.client/src/services/MessageService.js
@@ -1,10 +1,18 @@
 import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
 class MessageService {
     async getMessagesByChat(id){
         const res = await api.get('api/chat/' + id + '/messages')
         logger.log('getMessagesByChat', res.data)
         AppState.messages = res.data
+    }
+
+    async createMessage(body, id){
+        const res = await api.post('api/chat/' + id + '/messages', body)
+        logger.log('createmessage', res.data)
+        AppState.messages = [...AppState.messages, res.data]
     }
 }
 
